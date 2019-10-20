7ba871acd14d98c5ca491ea7a1b7163097437034
Budi Prasetya <budip@budip.com>
2019-10-20 10:58:34 -0700

    Field sensor data collector

---
+++ Raspberry_pi/mobile_sensor.py
@@ -0,0 +1,5 @@
+"""
+Connect and manage field sensor in the area or centralized end points. 
+Also collects local temperature, humidity, air pressure, movement (gyroscope)
+Comm method: wifi and mesh. Sat Comm, LoRa and GSM are still in development
+"""
